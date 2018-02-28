63329202e1121b16d9b68573bdd9e210f149c7a3
Preben Haukebøe <181394@stud.hvl.no>
2018-02-28 12:12:06 +0100

    Added som basic gameplay physics

---
+++ 2. RocketBoost/Assets/Rocket.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+	private Rigidbody rb;
+	private int ThrustForce;
+
+	public int Rotationspeed;
+	// Use this for initialization
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		ThrustForce = 1000;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		processInput();
+	}
+
+	void processInput()
+	{
+		if (Input.GetKey(KeyCode.Space))
+			rb.AddRelativeForce(Vector3.up*ThrustForce * Time.deltaTime);
+		if (Input.GetKey(KeyCode.S))
+			transform.Rotate(Vector3.forward, -Rotationspeed * Time.deltaTime);
+		else if (Input.GetKey(KeyCode.A))
+			transform.Rotate(Vector3.forward, Rotationspeed * Time.deltaTime);
+
+	}
+}
