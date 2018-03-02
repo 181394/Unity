bda3ebfbcd7938f4e1d6cc1ee766c3215b5883ea
Preben Haukebøe <181394@stud.hvl.no>
2018-03-02 08:12:17 +0100

    Small clean-up in code

--- 2. RocketBoost/Assets/Rocket.cs
+++ 2. RocketBoost/Assets/Rocket.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -23,27 +24,37 @@ public class Rocket : MonoBehaviour
 
 	void processInput()
 	{
-//        Thusters
-		if (Input.GetKey(KeyCode.Space))
-		{
-			rb.AddRelativeForce(Vector3.up*ThrustForce * Time.deltaTime);
-			if (!GetComponent<AudioSource>().isPlaying)
-				GetComponent<AudioSource>().Play();
-		}
-		else if (GetComponent<AudioSource>().isPlaying)
-			GetComponent<AudioSource>().Stop();
+		Thrust();
+		Rotate();
 
+	}
+
+	private void Rotate()
+	{
 		//        Rotation
 		if (Input.GetKey(KeyCode.S))
 			transform.Rotate(Vector3.forward, -Rotationspeed * Time.deltaTime);
 		else if (Input.GetKey(KeyCode.A))
 			transform.Rotate(Vector3.forward, Rotationspeed * Time.deltaTime);
 
-//        Restart With Key R
+		//        Restart With Key R
 		if (Input.GetKey(KeyCode.R))
 		{
 			transform.position = new Vector3(0, 3.043795f, 0);
 			transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
 		}
+	}
+
+	private void Thrust()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			rb.AddRelativeForce(Vector3.up * ThrustForce * Time.deltaTime);
+			if (!GetComponent<AudioSource>().isPlaying)
+				GetComponent<AudioSource>().Play();
+		}
+		else if (GetComponent<AudioSource>().isPlaying)
+			GetComponent<AudioSource>().Stop();
+
 	}
 }
