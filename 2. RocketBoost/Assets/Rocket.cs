6f54d7b7394f4dc87d338ed4582076a7f075454e
Preben Haukebøe <181394@stud.hvl.no>
2018-07-31 13:38:51 +0200

    Fixed some physics

--- 2. RocketBoost/Assets/Rocket.cs
+++ 2. RocketBoost/Assets/Rocket.cs
@@ -8,38 +8,84 @@ public class Rocket : MonoBehaviour
 	private Rigidbody rb;
 	private int ThrustForce;
 
-	public int Rotationspeed;
+	[SerializeField] float thrustForce = 1000f;
+	[SerializeField] float Rotationspeed = 142f;
 	// Use this for initialization
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
-		ThrustForce = 1000;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		processInput();
-	}
-
-	void processInput()
-	{
 		Thrust();
 		Rotate();
-
+		
 	}
+
+
+
+	void FixedUpdate()
+	{
+		Landing();
+	}
+
+
+
+
+	void OnCollisionEnter(Collision collision)
+	{
+		print("Crashed!");
+	}
+
+
+	void Landing()
+	{
+		if (Input.GetKey(KeyCode.L))
+		{
+			rb.freezeRotation = true;
+			print("LAND");
+			if (rb.rotation.z > 0)
+			{
+				for (float i = rb.rotation.z; i > 0; i-=0.02f)
+				{
+					print(i);
+					Vector3 eulVec = new Vector3(rb.rotation.x, rb.rotation.y, i);
+
+					Quaternion rotQuaternion = Quaternion.Euler(eulVec*Time.deltaTime);
+					rb.MoveRotation(rotQuaternion);
+				}
+			}else if (rb.rotation.z < 0)
+			{
+				for (float i = rb.rotation.z; i < 0; i+=0.02f)
+				{
+					print(i);
+					Vector3 eulVec = new Vector3(rb.rotation.x, rb.rotation.y, i);
+
+					Quaternion rotQuaternion = Quaternion.Euler(eulVec * Time.deltaTime);
+					rb.MoveRotation(rotQuaternion);
+				}
+			}
+		}
+	}
+
 
 	private void Rotate()
 	{
+		rb.freezeRotation = true;
 		//        Rotation
 		if (Input.GetKey(KeyCode.S))
 			transform.Rotate(Vector3.forward, -Rotationspeed * Time.deltaTime);
 		else if (Input.GetKey(KeyCode.A))
 			transform.Rotate(Vector3.forward, Rotationspeed * Time.deltaTime);
 
+		rb.freezeRotation = false;
+
 		//        Restart With Key R
 		if (Input.GetKey(KeyCode.R))
 		{
+			rb.freezeRotation = true;
 			transform.position = new Vector3(0, 3.043795f, 0);
 			transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
 		}
@@ -49,7 +95,7 @@ public class Rocket : MonoBehaviour
 	{
 		if (Input.GetKey(KeyCode.Space))
 		{
-			rb.AddRelativeForce(Vector3.up * ThrustForce * Time.deltaTime);
+			rb.AddRelativeForce(Vector3.up * thrustForce * Time.deltaTime);
 			if (!GetComponent<AudioSource>().isPlaying)
 				GetComponent<AudioSource>().Play();
 		}
